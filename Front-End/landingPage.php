98fa9e015fd6c1d71e2aedc32cb68a0bb63caa2c
Lemuel Bonite <ljbonite21@gmail.com>
2022-05-09 09:06:15 -0600

    added frontend php files

--- Front-End/landingPage.php
+++ Front-End/landingPage.php
@@ -1,5 +1,5 @@
 <?php
-echo "Hello world! This is the Landing Page";
+$test = "Hello world! This is the Landing Page";
 ?>
 
 <!DOCTYPE html>
@@ -8,8 +8,12 @@ echo "Hello world! This is the Landing Page";
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Content Library</title>
 </head>
+<header>
+    <?php include 'header.php' ?>
+</header>
 <body>
     
 </body>
