50c1d8de6ae78a1927f2aab67d71492e384142ad
Cole Damon <44043262+coledamon@users.noreply.github.com>
2022-05-09 19:38:52 -0600

    bfix

--- back-end/category/getCategories.php
+++ back-end/category/getCategories.php
@@ -10,9 +10,7 @@ function getCategories($dbConn) {
     $query = "SELECT JSON_OBJECT(
                 'id', c.id,
                 'Name', c.category_name,
-                'Used', c.used,
-                'Title', cp.title,
-                'Body', cp.body) as Category
+                'Used',  If(c.used, cast(true as json), cast(false as json))) as Category
                 FROM Category c";
 
     return @mysqli_query($dbConn, $query);
