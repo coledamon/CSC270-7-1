b427d1e3e7b4791d923640fbca33feb87b4e9408
Lemuel Bonite <ljbonite21@gmail.com>
2022-05-11 17:48:51 -0600

    cat

--- Front-End/categoryPage.php
+++ Front-End/categoryPage.php
@@ -2,8 +2,19 @@
 include "header.php";
 $name = $_GET['name'];
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="reset.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
     <title>
-        <?php echo $_GET["name"]." Category" ?>
+        <?php echo $_GET["name"] . " Category" ?>
     </title>
 </head>
 
@@ -13,27 +24,30 @@ $name = $_GET['name'];
         <?php echo $name ?>
     </h2>
 
-    <div class="d-flex">
-        <div id="wrapper" class="row justify-content-center m-4"></div>
+    <div class="d-flex justify-content-center">
+        <div id="wrapper" class="row  m-4"></div>
     </div>
+</body>
+
+</html>
 
 <script>
     const getCategoryPage = async () => {
         await fetch(`../back-end/category/getCategoryByName.php?name=<?php echo $_GET["name"] ?>`)
-        .then(res => res.json())
-        .then(data => {
-            console.log(data);
-            //take the data and display title && body && use name for header
-        });
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                //take the data and display title && body && use name for header
+            });
     }
 
     const getCategoryMedia = async () => {
         await fetch(`../back-end/media/getMediaByCategory.php?name=<?php echo $_GET["name"] ?>`)
-        .then(res => res.json())
-        .then(data => {
-            console.log(data);
-            displayMedia(data);
-        });
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                displayMedia(data);
+            });
     }
 
     getCategoryPage();
@@ -41,40 +55,40 @@ $name = $_GET['name'];
 
     const updateCategoryPage = () => {
         fetch("../back-end/category/updateCategoryPage.php", {
-            body: new URLSearchParams(new FormData(document.getElementById("updateCategoryPageForm"))).toString(),
-            headers: {
-                 "Content-Type": "application/x-www-form-urlencoded",
-            },
-            method: "post"
-        })
-        .then(res => res.json())
-        .then(data => {
-            console.log(data);
-        });
+                body: new URLSearchParams(new FormData(document.getElementById("updateCategoryPageForm"))).toString(),
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded",
+                },
+                method: "post"
+            })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+            });
     }
 
     const createMedia = () => {
         fetch("../back-end/media/createMedia.php", {
-            body: new URLSearchParams(new FormData(document.getElementById("createMediaForm"))).toString(),
-            headers: {
-                 "Content-Type": "application/x-www-form-urlencoded",
-            },
-            method: "post"
-        })
-        .then(res => res.json())
-        .then(data => {
-            console.log(data);
-        });
+                body: new URLSearchParams(new FormData(document.getElementById("createMediaForm"))).toString(),
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded",
+                },
+                method: "post"
+            })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+            });
     }
 
     const deleteMedia = (id) => {
         fetch(`../back-end/media/deleteMediaById.php?id=${id}`)
-        .then(res => res.json())
-        .then(data => {
-            console.log(data);
-        });
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+            });
     }
-    
+
     const displayMedia = (media) => {
         const wrapper = document.getElementById('wrapper');
         media.forEach(media => {
