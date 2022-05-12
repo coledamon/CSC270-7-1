ac785a1fee8bab8a1395e1ae3e9e89985e398252
Lemuel Bonite <ljbonite21@gmail.com>
2022-05-12 00:24:35 -0600

    show no records

--- Front-End/categoryPage.php
+++ Front-End/categoryPage.php
@@ -10,7 +10,7 @@ $name = $_GET['name'];
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="reset.css">
+    <!-- <link rel="stylesheet" href="reset.css"> -->
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
     <title>
@@ -91,9 +91,15 @@ $name = $_GET['name'];
 
     const displayMedia = (media) => {
         const wrapper = document.getElementById('wrapper');
+        const noRecords = document.createElement('h4');
+        noRecords.textContent = "No records found"
         media.forEach(media => {
-            const mediaDiv = createMediaCard(media.Title, media.Creator, media.Genre, media.Year, media.id);
-            wrapper.append(mediaDiv);
+            if (!media.error) {
+                const mediaDiv = createMediaCard(media.Title, media.Creator, media.Genre, media.Year, media.id);
+                wrapper.append(mediaDiv);
+            } else {
+                wrapper.append(noRecords);
+            }
         })
     }
 
